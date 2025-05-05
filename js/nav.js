727731ed0da04fc9917770d9f664cdbb2ecdfdb1
menu <mmenu9558@gmail.com>
2025-05-05 13:38:42 -0300

    actualizo

--- js/nav.js
+++ js/nav.js
@@ -3,12 +3,12 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("nav").innerHTML = `
         <nav class="menu">
             <div class="logo">
-                <img src="imagenes/camara-removebg-preview.png" alt="Logo" width="90">
+                <img src="imagenes/logo.png" alt="Logo" width="90">
             </div>
             <ul class="nav-links hidden" id="navLinks">
                 <li><a href="index.html">Inicio</a></li>
-                <li><a href="pagina1.html">Fotos</a></li>
-                <li><a href="pagina2.html">Contacto</a></li>
+                <li><a href="fotos.html">Fotos</a></li>
+                <li><a href="contacto.html">Contacto</a></li>
             </ul>
             <div class="boton-final">
                 <button id="toggleMenu">Menu</button>
